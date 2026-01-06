ff27eda7af23466937bb9c5bc7ef91791df87b1d
hussainali003 <hussainali9203@gmail.com>
2026-01-06 11:44:16 +0500

    add/remove: remove bootstrap and add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div className="container text-center mt-5">
-      <h1 className="text-primary">Bootstrap is working!</h1>
-    </div>
-  );
+  return <div className="text-3xl font-bold underline">helll</div>;
 }
 
 export default App;
