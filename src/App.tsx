cd54b2ce30e40f46d6be5de9e1ec98724db967b3
hussainali003 <hussainali9203@gmail.com>
2026-01-13 17:53:19 +0500

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "./components/AboutSection";
 import BackgroundContainer from "./components/BackgroundContainer";
 import ContactSection from "./components/ContactSection";
+import FooterSection from "./components/FooterSection";
 import Header from "./components/Header";
 import MainContainer from "./components/MainContainer";
 
@@ -12,6 +13,7 @@ function App() {
       <MainContainer />
       <AboutSection />
       <ContactSection />
+      <FooterSection />
     </div>
   );
 }
---
+++ src/components/FooterSection.tsx
@@ -0,0 +1,7 @@
+export default function FooterSection() {
+  return (
+    <div className="w-full mt-14 py-4 border-t border-gray-400/80 opacity-40">
+      <h3 className="text-sm text-gray-500 text-center dark:text-gray-400">© 2025 - All Rights Reserved.</h3>
+    </div>
+  )
+} 
